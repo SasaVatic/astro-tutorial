e21ce46f9c22b795f46b304b794695b0334da45b
Sasa Vatic <zeklbike@gmail.com>
2023-11-21 22:05:01 +0100

    building first astro island with react js

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://musical-pegasus-487492.netlify.app/"
+  site: "https://musical-pegasus-487492.netlify.app/",
+  integrations: [react()]
 });
