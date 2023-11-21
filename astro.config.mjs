f638af5a48f4bf4caa94569da4f38e12561409f8
Sasa Vatic <zeklbike@gmail.com>
2023-11-21 18:53:31 +0100

    adding an RSS feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://musical-pegasus-487492.netlify.app/"
+});
